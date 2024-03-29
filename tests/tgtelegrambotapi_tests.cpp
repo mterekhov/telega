9cf6875ed804d7b6e49fc51c94b0243794e4e10a
Michael Terekhov <m.terekhov@icloud.com>
2024-03-29 19:53:38 +0300

    adding first test

--- tests/tgtelegrambotapi_tests.cpp
+++ tests/tgtelegrambotapi_tests.cpp
@@ -1,9 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
 #include "tgtelegrambotapi.h"
 
-int main(int argc, const char *argv[]) {
+TEST_CASE("Testing Telegram bot API") {
     spcTelega::TGTelegramBotAPI telegramBot("6356062369:AAHGg0paAAwIaWX8sC-4S1LQECjskaesHb0");
     const std::string response = telegramBot.sendMessage("test message");
-    
-
-	return 0;
+	CHECK(!response.empty());
 }
